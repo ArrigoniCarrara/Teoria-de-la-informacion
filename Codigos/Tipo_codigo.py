fc3dea438c9d61122d513550bb4d1ba1dfc733f4
Gian Luca Arrigoni Carrara <gianlucaarrigoni06@gmail.com>
2026-09-13 12:32:51 -0300

    Add files via upload

---
+++ Codigos/Tipo_codigo.py
@@ -0,0 +1,77 @@
+def nosingular(cod):
+    if(len(cod) != len(set(cod))):
+        return False
+    return True
+
+def instantaneo(cod):    # IMPORTANTE LOS ELEMENTOS DE LA LISTA DEBEN ESTAR COMO STRING
+    if(nosingular(cod)):  # Lo corroboro igual por si se utiliza fuera del main
+        for i in range(len(cod)): # Un toque ineficiente pero no importa
+            for j in range(len(cod)):
+                if i != j: 
+                    if cod[i].startswith(cod[j]):
+                        return False # prefijo
+    else:
+        return False                
+    return True 
+
+def univocamente_decodificable(cod):
+
+    if(nosingular(cod)):
+
+        if(instantaneo(cod)): # Corroboro que sea singular y si es instantaneo devuelvo True
+            return True
+        
+        C = set(cod)
+        S1 = C 
+        conjuntos_vistos = [] # Lista para ir guardando los S_i
+        S_i = S1 
+
+        while True: #Ciclo Infinito hasta que haya un return
+            S_siguiente = set()
+        
+            for x in S1:
+                for y in S_i:
+
+                    if x != y:
+
+                        if y.startswith(x):
+                            sufijo = y[len(x):]
+                            S_siguiente.add(sufijo)
+                    
+                        elif x.startswith(y):
+                            sufijo = x[len(y):]
+                            S_siguiente.add(sufijo)
+        
+                
+            if not S_siguiente: # Si el conjunto de sufijos está vacio es UD.
+                return True
+            
+                
+            if S_siguiente & C: # Si algun S_i contiene una palabra de C, el código no es UD
+                return False
+            
+                
+            if S_siguiente in conjuntos_vistos: # Si se obtiene un S_i que ya aparecio antes, el código es UD
+                return True
+            
+                
+            conjuntos_vistos.append(S_siguiente) 
+            S_i = S_siguiente
+
+
+cod = [".,", ";", ",,", ":", "...", ",:;"]
+print(cod)
+
+if(nosingular(cod)):
+    print("Es No Singular")
+    if(instantaneo(cod)):
+        print("Es Univocamente Decodificable")
+        print("Es Instantaneo")
+    else:
+        if(univocamente_decodificable(cod)):
+            print("Es Univocamente Decodificable")
+        else:
+            print("No es Univocamente Decodificable")
+        print("No es Instantaneo")
+else:
+    print("Es Singular")
